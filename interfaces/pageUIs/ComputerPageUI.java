d8e2eba5b44da02ad26679d8639c990c87df3625
qhuynhda <quyen huynh>
2024-03-12 13:19:30 +0700

    Add TC_01,02,03 for Project NopCommerce

---
+++ interfaces/pageUIs/ComputerPageUI.java
@@ -0,0 +1,5 @@
+package pageUIs;
+
+public class ComputerPageUI {
+    public static final String DESKTOP_IMAGE="xpath=//a[contains(text(),'Desktop')]/parent::h2/following-sibling::div[@class='picture']";
+}
